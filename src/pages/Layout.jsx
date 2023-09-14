adbfca6063f6acb35e27d9770c3edcd2cb640b76
krokerdile <crazykroker@gmail.com>
2023-09-14 13:49:32 +0900

    feat: Apply Layout to Router

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,5 +1,23 @@
-const Layout = (children) => {
-  return <>{children}</>;
+import Navbar from "../Components/Navbar";
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+const Layout = () => {
+  return (
+    <LayoutWrapper>
+      <Navbar />
+      <Outlet />
+    </LayoutWrapper>
+  );
 };
 
 export default Layout;
+
+const LayoutWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+
+  width: 100%;
+  height: 100vh;
+`;
