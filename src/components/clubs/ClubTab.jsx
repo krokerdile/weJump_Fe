b70f2e91b67a55521a66acd1cb51fa3f887df2af
krokerdile <crazykroker@gmail.com>
2023-09-17 14:10:06 +0900

    feat: ClubDetail Tab Status 기준 변경 가능 기능 추가

---
+++ src/components/clubs/ClubTab.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import Text from "../common/Text";
+import useClubTabStore from "../../store/useClubTabStore";
+
+const ClubTab = () => {
+  const { setClubTabStatus, clubTabStatus } = useClubTabStore();
+
+  const handleTabStatus = (status) => {
+    setClubTabStatus(status);
+  };
+  return (
+    <TabBox>
+      <button onClick={() => handleTabStatus("Member")}>Member</button>
+      <button onClick={() => handleTabStatus("Notice")}>Notice</button>
+      <Text>{clubTabStatus}</Text>
+    </TabBox>
+  );
+};
+
+export default ClubTab;
+
+const TabBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: left;
+  gap: 1rem;
+`;
