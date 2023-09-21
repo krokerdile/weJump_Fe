37470a8f23c4c78537ce5be6eef0985b49b754da
krokerdile <crazykroker@gmail.com>
2023-09-22 03:10:55 +0900

    feat: Text 컴포넌트 수정

--- src/components/common/Text.jsx
+++ src/components/common/Text.jsx
@@ -1,8 +1,18 @@
 import styled from "styled-components";
 
-const Text = styled.p`
-  font-size: 1rem;
-  margin: 0;
-`;
+const Text = ({ text, fontSize, fontWeight, color, onClick }) => {
+  return (
+    <TextLine fontSize={fontSize} onClick={onClick} fontWeight={fontWeight} color={color}>
+      {text}
+    </TextLine>
+  );
+};
 
 export default Text;
+
+const TextLine = styled.p`
+  font-size: ${(props) => props.theme.fontSize[props.fontSize]};
+  font-weight: ${(props) => props.theme.fontWeight[props.fontWeight]};
+  color: ${(props) => props.theme.color[props.color]};
+  text-decoration: none;
+`;
