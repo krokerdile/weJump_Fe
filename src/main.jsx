ab4077a982d6dfac9c81cb2d9483822e80efe75b
krokerdile <crazykroker@gmail.com>
2023-09-21 22:37:38 +0900

    fix: ThemeProvider import문 수정

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter, ThemeProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
