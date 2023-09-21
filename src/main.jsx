44ae73b37cad00779e86bdd23494f94bdf6a9ddb
krokerdile <crazykroker@gmail.com>
2023-09-21 17:42:17 +0900

    feat: themeProvider 추가 - main.jsx themeProvider 추가 - theme.js에 fontSize, fontWeight, color 추가

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, ThemeProvider } from "react-router-dom";
+import theme from "./styles/theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/styles/theme.js
@@ -0,0 +1,30 @@
+const theme = {
+  fontSize: {
+    headline1: "2.6637rem",
+    headline2: "1.5rem",
+    category: "1.6687rem",
+    bodycopy: "1.5rem",
+    caption: "1.3331rem",
+  },
+  fontWeight: {
+    headline1: "bold",
+    headline2: "bold",
+    category: "bold",
+    bodycopy: "normal",
+    caption: "normal",
+  },
+  color: {
+    highlight: "#D3F1C5",
+    button: "#00D068",
+    undecided: "#008354",
+    pressed: "#005838",
+    white: "#FFFFFF",
+    grey1: "#E8E8E8",
+    grey2: "#B4B4B4",
+    grey3: "#545454",
+    black: "#000000",
+    negative: "EE0000",
+  },
+};
+
+export default theme;
