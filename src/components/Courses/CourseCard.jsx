fbf507341b8a2953a3c9a916ff0cdbd294d6d1c8
east_filmm <ehdvlf001@naver.com>
2023-10-12 02:53:12 +0900

    feat: add student count icon

--- src/components/Courses/CourseCard.jsx
+++ src/components/Courses/CourseCard.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Text from "../common/Text";
+import person from "./icon/user.png";
 
 const CourseCard = (props) => {
   return (
@@ -7,9 +8,10 @@ const CourseCard = (props) => {
       <CardCover src={props.img} alt="no" />
       <CardFooter>
         <Text text={props.courseName} fontSize="headline1" fontWeight="headline1" color="black" />
+        <Text text={props.mentorName} fontSize="headline2" fontWeight="headline2" color="black" />
         <Detail>
-            <Text text={props.mentorName} fontSize="headline2" fontWeight="headline2" color="black" />
-            <Text text={`${props.studentCount} / ${props.studentMaxNum}`} fontSize="caption" fontWeight="caption" color="black" />
+            <img src={person} alt="person" width="16" height="16" style={{ marginRight: "10px", marginTop:"8px" }}/>
+            <Text text={` ${props.studentCount} / ${props.studentMaxNum}`} fontSize="caption" fontWeight="caption" color="black" />
         </Detail>
       </CardFooter>
     </CardWrapper>
@@ -32,7 +34,8 @@ const CardFooter = styled.div`
   flex-direction: column;
   justify-content: left;
   text-align: left;
-  padding: 1rem;
+  padding: 0.4rem;
+  margin-left: 0.4rem;
 `;
 
 const CardCover = styled.img`
@@ -44,5 +47,6 @@ const CardCover = styled.img`
 `;
 
 const Detail = styled.div`
-
+  display: flex;
+  flex-direction: row;
 `;
